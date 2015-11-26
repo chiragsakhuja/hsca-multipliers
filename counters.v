f1407745c4ec1b7e4c2a6fb4a0f4afedaf6190e7
Chirag Sakhuja <dev.sakhuja@gmail.com>
2015-11-25 22:23:18 -0600

    Add 15:4 counter

--- counters.v
+++ counters.v
@@ -37,13 +37,35 @@ module counter7(in, out);
     wire [1:0] fa1_out;
     wire [1:0] fa2_out;
     wire [1:0] rca1_out;
-    wire [1:0] rca2_out;
 
     counter3 fa1(in[2:0], fa1_out);
     counter3 fa2(in[5:3], fa2_out);
-    counter3 rca1({in[6], fa1_out[0], fa2_out[0]}, rca1_out);
+
+    counter3 rca1({in[6]      , fa1_out[0], fa2_out[0]}, rca1_out);
     counter3 rca2({rca1_out[1], fa1_out[1], fa2_out[1]}, out[2:1]);
 
     assign out[0] = rca1_out[0];
+
+endmodule
+
+module counter15(in, out);
+
+    input wire  [14:0] in;
+    output wire [3:0] out;
+
+    wire [2:0] c1_out;
+    wire [2:0] c2_out;
+    wire [1:0] rca1_out;
+    wire [1:0] rca2_out;
+
+    counter7 c1(in[6:0] , c1_out);
+    counter7 c2(in[13:7], c2_out);
+
+    counter3 rca1({in[14]     , c1_out[0], c2_out[0]}, rca1_out);
+    counter3 rca2({rca1_out[1], c1_out[1], c2_out[1]}, rca2_out);
+    counter3 rca3({rca2_out[1], c1_out[2], c2_out[2]}, out[3:2]);
+
+    assign out[0] = rca1_out[0];
+    assign out[1] = rca2_out[0];
 
 endmodule
--- counters_tb.v
+++ counters_tb.v
@@ -4,11 +4,14 @@ module top;
     wire [1:0] dut_3_2_out;
     reg  [6:0] dut_7_3_in;
     wire [2:0] dut_7_3_out;
+    reg  [14:0] dut_15_4_in;
+    wire [3:0] dut_15_4_out;
 
     integer test_val;
 
     counter3 dut_3_2(dut_3_2_in, dut_3_2_out);
     counter7 dut_7_3(dut_7_3_in, dut_7_3_out);
+    counter15 dut_15_4(dut_15_4_in, dut_15_4_out);
 
     reg passed;
 
@@ -38,6 +41,23 @@ module top;
             #10 test_val = dut_7_3_in[0] + dut_7_3_in[1] + dut_7_3_in[2] + dut_7_3_in[3] + dut_7_3_in[4] + dut_7_3_in[5] + dut_7_3_in[6];
             if(dut_7_3_out != test_val) begin
                 $display("7:3 incorrect at %b (actual: %b, expected: %b)", dut_7_3_in, dut_7_3_out, test_val[2:0]);
+                passed = 1'b0;
+            end
+        end
+
+        if(passed == 1'b1) begin
+            #10 $display("Test passed.");
+        end else begin
+            #10 $display("Test failed.");
+        end
+
+        passed = 1'b1;
+
+        $display("Testing 15:4");
+        for(dut_15_4_in = 15'b000000000000000; dut_15_4_in != 15'b111111111111111; dut_15_4_in = dut_15_4_in + 15'b000000000000001) begin
+            #10 test_val = dut_15_4_in[0] + dut_15_4_in[1] + dut_15_4_in[2] + dut_15_4_in[3] + dut_15_4_in[4] + dut_15_4_in[5] + dut_15_4_in[6] + dut_15_4_in[7] + dut_15_4_in[8] + dut_15_4_in[9] + dut_15_4_in[10] + dut_15_4_in[11] + dut_15_4_in[12] + dut_15_4_in[13] + dut_15_4_in[14];
+            if(dut_15_4_out != test_val) begin
+                $display("15:4 incorrect at %b (actual: %b, expected: %b)", dut_15_4_in, dut_15_4_out, test_val[3:0]);
                 passed = 1'b0;
             end
         end
